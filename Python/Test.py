1170894dd3dcaf037a47c5bd21f5c650d506db03
X-PC <xvillanu@gmail>
2016-02-06 11:07:24 -0500

    Initial Commit of Python Project

---
+++ Python/Test.py
@@ -0,0 +1,25 @@
+def solution(A):
+    n = len(A)
+    result = 0
+    for i in xrange(n - 1):
+        if (A[i] == A[i + 1]):
+            result = result + 1
+    r = 0
+    for i in xrange(n):
+        count = 0
+        if (i > 0):
+            if (A[i - 1] != A[i]):
+                count = count + 1
+            else:
+                count = count - 1
+        if (i < n - 1):
+            if (A[i + 1] != A[i]):
+                count = count + 1
+            else:
+                count = count - 1
+        r = max(r, count)
+    return result + r
+
+
+x = solution([1,1,1,0,1,0,0])
+print (x)
